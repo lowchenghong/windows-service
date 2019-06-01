587da666d312cf6bb5c6d6ea650bc7ac6064a10c
lowch <lowchenghong@yahoo.com.sg>
2019-06-02 07:56:14 +0800

    Initial version

---
+++ WindowsService/Library.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+    }
+    }
+
+}
